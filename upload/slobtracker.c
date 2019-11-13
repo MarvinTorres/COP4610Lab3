345eb9a646a8d4fd7f2754a1f4964f382e7fc013
root <mtorr068@fiu.edu>
2019-11-13 18:32:45 -0500

    update slobtracker.c

--- upload/slobtracker.c
+++ upload/slobtracker.c
@@ -1,13 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <linux/kernel.h>
 #include <sys/syscall.h>
 #include <unistd.h>
 
+#define NUM_BYTES 10000000000
+
 int main()
 {
+   int i;
+   int* a;
+   int* b;
+   int* c;
+   for (i = 0; i < 10000; i++) {
+	a = (int*)malloc(255);
+	b = (int*)malloc(0);
+	c = (int*)malloc(0);
 	long int amma = syscall(543);
 	printf("System call sys_get_slob_amt_claimed returned %ld\n", amma);
 	amma = syscall(544);
 	printf("System call sys_get_slob_amt_free returned %ld\n", amma);
+   }
+	free(a);
+	free(b);
+	free(c);
 	return 0;
 }
